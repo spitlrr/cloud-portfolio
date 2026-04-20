bc1d8e5d8afe485aa6afe292bb72cdde05a57081
spitlrr <187810628+spitlrr@users.noreply.github.com>
2026-04-20 15:09:38 -0500

    Clean deployment

--- main.tf
+++ main.tf
@@ -47,3 +47,84 @@ resource "aws_iam_role_policy_attachment" "admin_access" {
 output "role_arn" {
   value = aws_iam_role.github_actions_role.arn
 }
+
+# 1. Create the Origin Access Control (The Security Gatekeeper)
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "s3-portfolio-oac"
+  description                       = "OAC for Portfolio S3 Bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# 2. Create the CloudFront Distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.portfolio_bucket.bucket_regional_domain_name
+    origin_id                = "S3Origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3Origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https" # Security: Force Encryption
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # This is required even if you aren't restricting by country
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  # For now, we use the default CloudFront certificate
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# 3. Output the URL so you can find your site
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+# This policy allows CloudFront to reach into the bucket
+resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
+  bucket = aws_s3_bucket.portfolio_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.portfolio_bucket.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
+    ]
+  })
+}
